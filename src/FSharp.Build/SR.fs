8efb1e86052d7ffcd38efa275e481081e02f52a0
Enrico Sada <enrico@sada.io>
2017-01-09 18:40:38 +0100

    add FSharp.Build sources

---
+++ src/FSharp.Build/SR.fs
@@ -0,0 +1,11 @@
+namespace FSBuild
+
+  module SR =
+
+    let toolpathUnknown () = "ToolPath is unknown; specify the path to fsc.exe as the ToolPath property."
+
+namespace Internal.Utilities
+
+  module FSharpEnvironment =
+
+    let BinFolderOfDefaultFSharpCompiler = None
